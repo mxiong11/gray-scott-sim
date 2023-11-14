60803818ff51edc9240f1b8ffb079ab7aff64212
ming <13928124235@163.com>
2023-11-14 15:27:33 +0000

    Clean up redundant files

--- build/extern/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/13928/Desktop/Professional Skills/gray-scott-sim/extern/googletest/googletest
-# Build directory: C:/Users/13928/Desktop/Professional Skills/gray-scott-sim/build/extern/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
